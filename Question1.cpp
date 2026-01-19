54b0b0b612e7b86e5673c018f88c1277b54a5b78
asthapatel0716 <asthapatel0716@gmail.com>
2026-01-19 11:09:28 +0530

    Create Question1.cpp

---
+++ Question1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float amt1,amt2;
+    cout<<"Enter the two amounts: ";
+    cin>>amt1>>amt2;
+    cout<<"The sum of the two amounts is "<<amt1+amt2;
+    return 0;
+}
