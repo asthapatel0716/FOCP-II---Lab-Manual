d804a352b8e78fce07204154ce56046f5dc90d11
asthapatel0716 <asthapatel0716@gmail.com>
2026-01-19 11:10:41 +0530

    Create ClassQuestions1.cpp

---
+++ ClassQuestions1.cpp
@@ -0,0 +1,35 @@
+// Printing hello world
+
+/*#include<iostream>
+int main(){
+    std::cout<<"Hello World";
+    return 0;
+}*/
+
+// Taking an input and printing it
+
+/*#include<iostream>
+int main(){
+    int no;
+    std::cout<<"Enter an integer ";
+    std::cin>>no;
+    std::cout<<"The no is"<<no;
+    return 0;
+
+}*/
+
+//Reading marks of three subjects and printing the total
+
+/*#include<iostream>
+using namespace std;
+int main(){
+    int m1,m2,m3;
+    cout<<"Enter marks of first subject: ";
+    cin>>m1;
+    cout<<"Enter marks of second subject: ";
+    cin>>m2;
+    cout<<"Enter marks of third subject: ";
+    cin>>m3;
+    cout<<"The grand total is "<<m1+m2+m3;
+    return 0;
+}*/
