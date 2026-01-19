0ecbcdf7abafbf344c9f51450b8cd3b56e5a3e9e
asthapatel0716 <asthapatel0716@gmail.com>
2026-01-19 11:17:35 +0530

    Create Question2.cpp

---
+++ Question2.cpp
@@ -0,0 +1,10 @@
+// Accept three numbers and compute their average
+#include<iostream>
+using namespace std;
+int main(){
+    float marks1,marks2,marks3;
+    cout<<"Enter the marks of three students ";
+    cin>>marks1>>marks2>>marks3;
+    cout<<"The average of the three students is "<<(marks1+marks2+marks3)/3;
+    return 0;
+}
