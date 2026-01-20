bf666072a65ae204a90e20c53fb9aeb68b2b567d
asthapatel0716 <asthapatel0716@gmail.com>
2026-01-20 13:48:47 +0530

    Create Question2.cpp

---
+++ Question3.cpp
@@ -0,0 +1,11 @@
+//compute the area of the circle
+#include<iostream>
+using namespace std;
+int main(){
+    float radius;
+    cout<<"Enter the area of circle: ";
+    cin>>radius;
+    cout<<"The area of the circle is "<<(4*3.14*radius*radius);
+    return 0;
+}
+
