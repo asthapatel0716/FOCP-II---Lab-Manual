aa91e99884af936229419f008d47bb5c75574ab0
asthapatel0716 <asthapatel0716@gmail.com>
2026-02-15 23:44:22 +0530

    Create ClassPracticeQuestion2.cpp

---
+++ ClassPracticeQuestion2.cpp
@@ -0,0 +1,14 @@
+//Function for welcome user
+
+
+/* #include<iostream>
+using namespace std;
+void welcome(string fname="Guest",string lname=""){
+ cout<<"\nWelcome "<<fname<<" "<<lname;
+}
+int main(){
+    welcome();
+    welcome("Aastha");
+    welcome("Aastha,Patel");
+    return 0;
+}*/
