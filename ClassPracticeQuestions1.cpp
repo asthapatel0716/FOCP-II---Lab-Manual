ad74cb7ce16a68447efc7d54071add4b17429c7f
asthapatel0716 <asthapatel0716@gmail.com>
2026-02-15 23:43:18 +0530

    Create ClassPracticeQuestions1.cpp

---
+++ ClassPracticeQuestions1.cpp
@@ -0,0 +1,14 @@
+// a student can give minimum 2 test and maximum 5 test, a function to display the result
+/*#include<iostream>
+using namespace std;
+int result(int a, int b, int c=0, int d=0, int e=0){
+    int sum=a+b+c+d+e;
+    return sum;
+}
+int main(){
+    cout<<"\nResult : "<<result(87,98);
+    cout<<"\nResult : "<<result(87,98,55);
+    cout<<"\nResult : "<<result(87,98,64,90);
+    cout<<"\nResult : "<<result(87,98,43,88,95);
+    return 0;
+}*/
