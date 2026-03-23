13667b79445e41388b08d5fd9a2a5b4fb8597179
asthapatel0716 <asthapatel0716@gmail.com>
2026-03-23 23:27:07 +0530

    Create Question16.cpp

---
+++ Question16.cpp
@@ -0,0 +1,29 @@
+//accept a number and check whether it is prime or not
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    bool isPrime=true;
+    if(n<=1){
+        isPrime=false;
+    }
+    else{
+        for(int i=2;i<=n/2;i++){
+            if(n%i==0){
+                isPrime=false;
+                break;
+            }
+        }
+    }
+    if(isPrime){
+        cout<<"It is a prime number";
+    }
+    else{
+        cout<<"It is not a prime number";
+    }
+    return 0;
+}
